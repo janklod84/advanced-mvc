974649c560a7681c15278fc80f699f541a99db11
janklod84 <jeanyao34@yahoo.com>
2019-04-17 15:18:54 +0500

    setting somes constants in the index.php

--- index.php
+++ index.php
@@ -1,3 +1,36 @@
 <?php 
+/*
+ | ------------------------------------------------------------
+ |                    STARTING SESSION
+ | ------------------------------------------------------------
+*/
+session_start();
 
-echo 'Welcome to JanKlod';
+/*
+ | ------------------------------------------------------------
+ |               CONSTANTES DEFINITIONS
+ | ------------------------------------------------------------
+*/
+
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(__FILE__));
+
+
+/*
+ | ------------------------------------------------------------
+ |              GET URL
+ |              Exemple this path '/users/register/567'
+ |              will be trim left by '/' and we obtain  'users/register/567'
+ | ------------------------------------------------------------
+*/
+
+ $url = isset($_SERVER['PATH_INFO']) ? explode('/', ltrim($_SERVER['PATH_INFO'], '/')) : [];
+
+
+/*
+ | ------------------------------------------------------------
+ |              REQUIRING CORE FILES
+ | ------------------------------------------------------------
+*/
+
+ require_once(ROOT. DS . 'core' . DS . 'bootstrap.php');
