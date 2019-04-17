596c2778fc71bdae3fb13bf4c03465fff7213543
janklod84 <jeanyao34@yahoo.com>
2019-04-18 00:14:55 +0500

    View class Part 2

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -4,13 +4,25 @@
 class Home extends Controller 
 {
 
-      
+      /**
+       * Constructor
+       * @param string $controller 
+       * @param string $action 
+       * @return void
+      */
+      public function __construct($controller, $action)
+      {
+           parent::__construct($controller, $action);
+      }
+
+
       /**
        * Render view index
        * @return mixed
       */
-	  public function indexAction()
+	  public function indexAction($name)
 	  {
-
+	  	  echo $name;
+          $this->view->render('home/index');
 	  }
 }
--- core/View.php
+++ core/View.php
@@ -17,7 +17,7 @@ class View
 		 /**
 		  * @var string
 		 */
-		 protected $siteTitle;
+		 protected $siteTitle = SITE_TITLE;
 
 	     
 	     /**
@@ -53,10 +53,16 @@ class View
              $viewString = implode(DS, $viewArray);
 
              $viewPath = ROOT . DS . 'app' . DS . 'views' . DS . $viewString . '.php';
+             $layoutPath = ROOT . DS . 'app' . DS . 'views' . DS . 'layouts' . DS . $this->layout . '.php';
+
              if(file_exists($viewPath))
              {
              	include($viewPath);
-             	include(ROOT . DS . 'app' . DS . 'views' . DS . 'layouts' . DS . $this->layout . '.php');
+             	
+             	if(file_exists($layoutPath))
+             	{
+             		 include($layoutPath);
+             	}
 
              }else{
 
@@ -82,5 +88,70 @@ class View
              }
 
              return false;
+		 }
+
+         
+         /**
+          * start type
+          * @param string $type 
+          * @return void
+         */
+		 public function start($type)
+		 {
+             $this->outputBuffer = $type;
+             ob_start();
+		 }
+         
+
+         /**
+          * end started part
+          * @return void
+         */
+		 public function end()
+		 {
+		 	 if($this->outputBuffer == 'head')
+		 	 {
+		 	 	  $this->head = ob_get_clean();
+
+		 	 }elseif($this->outputBuffer == 'body'){
+
+		 	 	  $this->body = ob_get_clean();
+
+		 	 }else{
+
+		 	 	 die('You must first run the start method.');
+		 	 }
+		 }
+
+
+         /**
+          * Render site title
+          * @return string
+         */
+         public function siteTitle()
+         {
+             return $this->siteTitle;
+         }
+
+
+         /**
+          * set site title
+          * @param string $title 
+          * @return void
+         */
+		 public function setSiteTitle($title)
+		 {
+		 	  $this->siteTitle = $title;
+		 }
+
+
+		 /**
+          * set layout
+          * @param string $path 
+          * @return void
+         */
+		 public function setLayout($path)
+		 {
+		 	 $this->layout = $path;
 		 }
 }
