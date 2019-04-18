bf6079085b6e509149a9ac5f17bc26166a3443b6
janklod84 <jeanyao34@yahoo.com>
2019-04-18 07:16:30 +0500

    Model class Part 2

--- core/Model.php
+++ core/Model.php
@@ -85,4 +85,70 @@ class Model
 		    {
 		    	 return $this->db->get_columns($this->table);
 		    }
+
+            
+            /**
+             * Find item with parses params
+             * Extrating params
+             * 
+             * @param array $params 
+             * @return array
+            */
+		    public function find($params = [])
+		    {
+                 $results = [];
+                 $resultsQuery = $this->db->find($this->table, $params);
+
+                 foreach($resultsQuery as $result)
+                 {
+                 	   $obj = new $this->modelName($this->table);
+                 	   $obj->populateObjData($result);
+                 	   $results[] = $obj;
+                 }
+
+                 return $results;
+		    }
+
+
+            
+            /**
+             * Find first record
+             * @param array $params 
+             * @return array
+            */
+		    public function findFirst($params = [])
+		    {
+		    	 $resultsQuery = $this->db->findFirst($this->table, $params);
+		    	 $result = new $this->modelName($this->table);
+                 $result->populateObjData($resultsQuery);
+                 return $result;
+		    }
+
+            
+            /**
+             * Find item by id
+             * @param int $id 
+             * @return mixed
+            */
+		    public function findById($id)
+		    {
+                 return $this->findFirst([
+                     'conditions' => "id = ?",
+                     'bind' => [$id]
+                 ]);
+		    }
+
+            
+            /**
+             * Populate object data
+             * @param object $obj 
+             * @return void
+            */
+		    protected function populateObjData($result)
+		    {
+                 foreach($result as $key => $value)
+                 {
+                 	   $this->{$key} = $val;
+                 }
+		    }
 }
